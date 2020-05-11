7a78c70971579f1bbd7af9c9ceb2d3fe431f5c3b
Nishant Kumar <52008914+NK01@users.noreply.github.com>
2020-05-11 13:55:43 +0900

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*  This code will calculate dissolution in the two phase 
+    system. We will utilize moving boundary algorithm
+    and Concentration dependent Diffusivity.
+
+    C++ standard used = c++17
+    It is cross-platform capable code
+    compiler used = clang (llvm 10.0)
+
+    Copyright = Nishant Kumar (2020)
+*/
+int main()
+{
+    
+
+    return 0;
+}
